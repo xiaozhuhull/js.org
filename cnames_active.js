94c7b126dc7a257ff37efae0102c89fb589248fc
xiaozhuhull <87590906+xiaozhuhull@users.noreply.github.com>
2022-09-22 19:27:07 +0800

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3306,6 +3306,7 @@ var cnames_active = {
   "zyh": "zyh-chopper.github.io/zyh",
   "zykj": "cname.vercel-dns.com", // noCF
   "zyy": "zyyou.github.io/notes"
+  "clgx888": "cdnkd0s.cdn.xshost.co"
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
